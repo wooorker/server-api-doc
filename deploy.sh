007ce134a5375a3ffc72d05adae06b4f91a2c9ce
tigerb <624661874@qq.com>
2017-05-30 12:23:58 +0800

    docs: deploy

--- deploy.sh
+++ deploy.sh
@@ -6,5 +6,8 @@ git pull origin master
 # generate api doc
 cd ./apib && snowboard html -i ./v1/index.apib -o ./v1/index.html
 
-# mock api
-snowboard mock -i ./v1/index.apib -b 'api-mock.smartwork.mob:8080'
+# kill old mock api process
+sudo kill $(ps -aux | grep "snowboard" | awk '{print $2}' | head -n 1)
+
+# new mock api
+nohup sudo snowboard mock -i ./v1/index.apib -b 'api-mock.smartwork.mob:8080' &
---
+++ main.go
@@ -0,0 +1,13 @@
+package main
+
+import(
+    "fmt"
+)
+
+func hello()  {
+    fmt.Println("hello go")
+}
+
+func main()  {
+    hello()
+}
